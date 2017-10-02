df35fd08f50fc749ef05d9ce98c650281069a0b8
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-02 20:00:08 +0300

    2_Örnek_proje

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, toplam;
+
+            Console.Write("ilk degeri girin");
+            a = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("ikinci degeri girin");
+            b = Convert.ToInt6(Console.ReadLine());
+
+            toplam = a + b;
+            Console.WriteLine();
+
+            Console.WriteLine("{0} ve {1} sayıların toplamı : {2}",a,b(a + b));
+
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
