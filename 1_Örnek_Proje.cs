946dec645778e50e8f3144ea9aea07a01bd416bf
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-02 20:06:16 +0300

    1_Örnek_Proje

---
+++ 1_Örnek_Proje.cs
@@ -0,0 +1,23 @@
+﻿
+      using System; 
+
+namespace omer{
+
+   class BirinciOrnek{
+
+   		static void Main(String[] args)
+   		{
+   			string ad, soyad ; 
+   			ad = "Hüseyin"; 
+   			soyad = "Kuzu";
+
+   			Console.WriteLine(ad + " " + soyad); 
+
+   			Console.ReadKey(); 
+
+   		}
+
+
+   }
+
+}
