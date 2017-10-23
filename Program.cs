e0bfc7130158ffd1ee2d9bd08b048e10893377a7
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-23 19:31:50 +0300

    AsalMi.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("girdiğiniz sayıya kadar olan asal sayıları yazar :");
+            int sayi = Convert.ToInt16(Console.ReadLine());
+            bool asalmi = true;
+
+            for (int i = 2; i <= sayi; i++)
+            {
+                if (i % j == 0)
+                {
+                    asalmi = false;
+                    break;
+                }
+            }
+            if (asalmi == true)
+
+                Console.Write(i + "");
+            asalmi = true;
+        }
+
+
+
+        }
+    }
+}
