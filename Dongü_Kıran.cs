eb60eb4549f2216f4612d8ff93c9c147cbdac48b
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-23 19:30:35 +0300

    Update Dongü_Kıran.cs

--- Dongü_Kıran.cs
+++ Dongü_Kıran.cs
@@ -1,19 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace Dongü_Kıran
+namespace DonguKiran
 {
     class Program
     {
         static void Main(string[] args)
         {
-            for(char k='A'; k<='Z';k++)
+            
+            for(char k ='A'; k <= 'Z'; k++)
+            {
+                if (k == 'R')
+                    break;
+                Console.Write(k + " ");
+            }
+
+            int x =0, y=0;
+            while(x < 10)
+            {
+              Console.WriteLine("x {0} iken y {1}",x,y);
+                x++;
+                continue;
+                y++;
+            }
+
+            int a = 10, b = 20;
+            Console.WriteLine("{0} + {1} = {2}",a,b,a+b);
+            goto son;
+
+            Console.WriteLine("{0}* {1} = {2}",a,b,a*b);
+
+            son: Console.WriteLine("Program Bitti");
         }
-        if (k== 'R')
-        break;
-        Console.Write( k + " ");
     }
 }
