2d881361d165c4f0bcb74a69c68c2620b88ce6ef
Penerovic-huseyn-kuzu <32458792+Penerovic-huseyn-kuzu@users.noreply.github.com>
2017-10-23 19:15:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dongü_Kıran
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(char k='A'; k<='Z';k++)
+        }
+        if (k== 'R')
+        break;
+        Console.Write( k + " ");
+    }
+}
